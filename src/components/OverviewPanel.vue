581f69847a1fa77f257b32be692155b55ac12f7b
malt9830 <malt9830@stud.kea.dk>
2023-08-27 21:52:10 +0200

    Restyled mobile overview panel

--- src/components/OverviewPanel.vue
+++ src/components/OverviewPanel.vue
@@ -1,10 +1,10 @@
 <template>
   <aside>
-    <div class="sm:fixed w-full sm:max-w-[13rem] grid grid-cols-2 sm:grid-cols-1 gap-4 p-4 text-center">
+    <div class="sm:fixed w-full sm:max-w-[13rem] grid grid-cols-2 sm:grid-cols-1 sm:gap-4 p-4 text-center">
       <div class="flex flex-col justify-center">
         <div>
-          <h1 class="text-2xl sm:text-3xl mb-2">Hogwarts</h1>
-          <h2 class="text-lg sm:text-xl mb-2">Class of 1991</h2>
+          <h1 class="text-3xl sm:text-4xl mb-2">Hogwarts</h1>
+          <h2 class="text-xl sm:text-2xl mb-2">Class of 1991</h2>
         </div>
         <div>
           <h2 class="text-lg sm:text-xl mb-2">Students</h2>
@@ -18,7 +18,7 @@
               <p class="text-2xl grid-center z-10">{{ expelledStudents.length }}</p>
             </span>
           </div>
-          <div class="flex justify-center mb-2">
+          <div class="flex justify-center">
             <span v-for="house in houses" :key="house" class="w-8 h-8 inline-grid place-content-center">
               <img :src="`/images/crests/${house}-crest.svg`" class="w-auto h-8 grid-center opacity-50">
               <p class="text-2xl grid-center z-10">{{ studentsCount[house].length }}</p>
@@ -27,7 +27,7 @@
         </div>
       </div>
       <img src="../assets/crests/hogwarts-crest.svg" alt="Hogwarts Crest" class="w-full">      
-      <h3 class="text-2xl col-span-2 sm:col-span-1 sm:row-start-2 sm:row-end-2">Currently Displayed: {{ students.length }}</h3>
+      <h3 class="text-2xl mt-4 sm:mt-0 col-span-2 sm:col-span-1 sm:row-start-2 sm:row-end-2">Currently Displayed: {{ students.length }}</h3>
     </div>
   </aside>
 </template>
