69a10e15792ef89245559c9267ccebb31379bbc2
malt9830 <malt9830@stud.kea.dk>
2023-08-29 01:34:16 +0200

    Simplified students array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="min-h-screen bg-hogwarts-light text-hogwarts-accent font-merinda">
     <section class="max-w-6xl mx-auto grid sm:grid-cols-[13rem_auto]">
-        <OverviewPanel :students="allStudents"/>
+        <OverviewPanel :students="students"/>
         <StudentList :students="students"/>
     </section>
   </main>
@@ -13,16 +13,12 @@ import families from './assets/families.json'
 import formatStudent from './composables/reformatting.js'
 
 const familyNames = ref({...families})
-const allStudents = ref([])
-
-const students = computed(() => {
-  return allStudents.value
-})
+const students = ref([])
 
 onMounted(() => {
   studentsData.forEach((student) => {
     const formattedStudent = formatStudent(student, studentsData, familyNames.value)
-    allStudents.value.push(formattedStudent)
+    students.value.push(formattedStudent)
   })
 })
 </script>
