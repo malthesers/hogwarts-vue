1843cd069b90794c27d94aed1771305501899775
malt9830 <malt9830@stud.kea.dk>
2023-08-29 01:27:29 +0200

    Small fixes and changes
    - Reduced space for search, filter and sorting icons
    - Filtering now closes when method is chosen
    - Fixed incorrect max-height causing failed transition

--- src/components/StudentList.vue
+++ src/components/StudentList.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="p-4">
     <div class="mb-4">
-      <div class="grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[7rem_1fr] mb-4">
-        <input v-model="search" placeholder="Search..." class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2 outline-none">
+      <div class="text-xl grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[5rem_1fr] mb-4">
+        <input v-model="search" placeholder="Search..." class="bg-hogwarts-dark text-hogwarts-accent border-2 border-hogwarts-accent p-2 outline-none">
         <img src="../assets/icons/magnifying-wand.svg" alt="magnifying wand" class="h-12 place-self-center sm:row-start-1 sm:col-start-1">
       </div>
-      <div class="grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[7rem_1fr] mb-4">
+      <div class="text-xl grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[5rem_1fr] mb-4">
         <div class="cursor-pointer">
-          <p @click="showFilteringMethods = !showFilteringMethods" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2 flex justify-between items-center">
+          <p @click="showFilteringMethods = !showFilteringMethods" class="bg-hogwarts-dark text-hogwarts-accent border-2 border-hogwarts-accent p-2 flex justify-between items-center">
             <span>Filter by...</span>
             <img src="../assets/icons/chevron.svg" alt="chevron" :class="[ showFilteringMethods ? 'rotate-180' : 'rotate-0']" class="h-4 duration-300">
           </p>
           <Transition name="slide">
-            <div v-if="showFilteringMethods" class="max-h-68 overflow-hidden">
-              <p v-for="method in filteringMethods" :key="method.key" @click="filter = method.key" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2">
+            <div v-if="showFilteringMethods" class="max-h-72 overflow-hidden">
+              <p v-for="method in filteringMethods" :key="method.key" @click="filter = method.key, showFilteringMethods = false" class="bg-hogwarts-dark text-hogwarts-accent border-2 border-hogwarts-accent p-2">
                 <span>{{ method.name }}</span>
               </p>
             </div>
@@ -21,15 +21,15 @@
         </div>
         <img src="../assets/icons/badge.svg" alt="magnifying wand" class="h-12 place-self-center sm:row-start-1 sm:col-start-1">
       </div>
-      <div class="grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[7rem_1fr]">
+      <div class="text-xl grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[5rem_1fr]">
         <div class="cursor-pointer">
-          <p @click="showSortingMethods = !showSortingMethods" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2 flex justify-between items-center">
+          <p @click="showSortingMethods = !showSortingMethods" class="bg-hogwarts-dark text-hogwarts-accent border-2 border-hogwarts-accent p-2 flex justify-between items-center">
             <span>Sort by...</span>
             <img src="../assets/icons/chevron.svg" alt="chevron" :class="[ showSortingMethods ? 'rotate-180' : 'rotate-0']" class="h-4 duration-300">
           </p>
           <Transition name="slide">
             <div v-if="showSortingMethods" class="max-h-40 overflow-hidden">
-              <p v-for="method in sortingMethods" :key="method.key" @click="sorting = method.key" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2 flex justify-between items-center">
+              <p v-for="method in sortingMethods" :key="method.key" @click="sorting = method.key" class="bg-hogwarts-dark text-hogwarts-accent border-2 border-hogwarts-accent p-2 flex justify-between items-center">
                 <span>{{ method.name }}</span>
                 <img src="../assets/icons/chevron.svg" alt="chevron" :class="[ sorting === method.key ? 'rotate-180' : 'rotate-0' ]" class="h-4 duration-300">
               </p>
