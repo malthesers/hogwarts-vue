e1c21e374821b6454a1df9e8330f5510a0ce0f13
malt9830 <malt9830@stud.kea.dk>
2023-08-29 00:57:32 +0200

    Added initial filtering

--- src/components/StudentList.vue
+++ src/components/StudentList.vue
@@ -6,7 +6,19 @@
         <img src="../assets/icons/magnifying-wand.svg" alt="magnifying wand" class="h-12 place-self-center sm:row-start-1 sm:col-start-1">
       </div>
       <div class="grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[7rem_1fr] mb-4">
-        <input v-model="search" placeholder="Search..." class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2">
+        <div class="cursor-pointer">
+          <p @click="showFilteringMethods = !showFilteringMethods" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2 flex justify-between items-center">
+            <span>Filter by...</span>
+            <img src="../assets/icons/chevron.svg" alt="chevron" :class="[ showFilteringMethods ? 'rotate-180' : 'rotate-0']" class="h-4 duration-300">
+          </p>
+          <Transition name="slide">
+            <div v-if="showFilteringMethods" class="max-h-60 overflow-hidden">
+              <p v-for="method in filteringMethods" :key="method.key" @click="filter = method.key" class="bg-hogwarts-dark text-hogwarts-accent text-xl border-2 border-hogwarts-accent p-2">
+                <span>{{ method.name }}</span>
+              </p>
+            </div>
+          </Transition>
+        </div>
         <img src="../assets/icons/badge.svg" alt="magnifying wand" class="h-12 place-self-center sm:row-start-1 sm:col-start-1">
       </div>
       <div class="grid gap-2 grid-cols-[auto_3rem] sm:grid-cols-[7rem_1fr]">
@@ -40,10 +52,32 @@ const props = defineProps({
   students: Array
 })
 
+const showExpelled = ref(false)
+const sorting = ref('firstName')
+const filter = ref('All Students')
 const search = ref('')
 
+const showFilteringMethods = ref(false)
+const filteringMethods = ref([
+  {
+    key: '*',
+    name: 'All Students'
+  },
+  {
+    key: 'captain',
+    name: 'Captains'
+  },
+  {
+    key: 'prefect',
+    name: 'Prefects'
+  },
+  {
+    key: 'inquisitor',
+    name: 'Inquisitors'
+  },
+])
+
 const showSortingMethods = ref(false)
-const sorting = ref('firstName')
 const sortingMethods = ref([
   {
     key: 'firstName',
@@ -61,7 +95,11 @@ const sortingMethods = ref([
 
 const filteredStudents = computed(() => {
   let filteredStudents = []
+  // Show either active or expelled students
+  filteredStudents = props.students.filter(student => student.expelled === showExpelled.value)
+  // Includes search query
   filteredStudents = props.students.filter(student => student.fullName.toLowerCase().includes(search.value.toLowerCase()) || search.value === '')
+  // Sorts by chosen sorting method
   filteredStudents.sort((a, b) => { return a[sorting.value] > b[sorting.value] ? 1 : -1 })
 
   return filteredStudents
