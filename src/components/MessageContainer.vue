c52af6b7b976c297bf94cb9b2d69ad5f8e0328bb
malt9830 <malt9830@stud.kea.dk>
2023-09-01 06:01:04 +0200

    Relocated message formatting to messages.js

--- src/components/MessageContainer.vue
+++ src/components/MessageContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed z-10 top-0 left-0 w-full h-full p-4 pointer-events-none">
     <TransitionGroup name="slide" tag="div" class="relative max-w-2xl mx-auto">
-      <div v-for="message in formattedMessages" :key="message" class="absolute w-full p-2 flex justify-between bg-hogwarts-dark border-hogwarts-accent border-2 rounded-lg">
+      <div v-for="message in messages" :key="message" class="absolute w-full p-2 flex justify-between bg-hogwarts-dark border-hogwarts-accent border-2 rounded-lg">
         <div>
           <p class="text-md sm:text-2xl">{{ message.title }}</p>
           <p class="text-sm sm:text-lg">{{ message.description }}</p>
@@ -14,41 +14,7 @@
 
 <script setup>
 import useMessages from '../composables/messages.js'
-
 const { messages } = useMessages()
-
-const titles = ref({
-  gender: 'Student was not made prefect!',
-  house: 'Student was not made prefect!',
-  inquisitor: 'Student was not made an inquisitor!',
-  hacking: 'Student was not made an inquisitor!',
-  expulsion1: 'Student could not be expelled!',
-  expulsion2: 'Student could not be expelled!',
-  expulsion3: 'Student could not be expelled!'
-})
-
-const descriptions = ref({
-  gender: 'already has a prefect of that gender.',
-  house: 'already has both of their prefects.',
-  inquisitor: 'is neither a Slytherin nor pure of blood.',
-  hacking: 'has been removed as an inquisitor.',
-  expulsion1: 'Do not try to expel this student again.',
-  expulsion2: 'I said do NOT try to expel this student again!',
-  expulsion3: 'This is the last warning. Do NOT expel this student!'
-})
-
-const formattedMessages = computed(() => {
-  let formattedMessages = []
-
-  messages.value.forEach((msg) => {
-    formattedMessages.push({
-      title: titles.value[msg.type],
-      description: `${msg.query} ${descriptions.value[msg.type]}`
-    })
-  })
-
-  return formattedMessages
-})
 </script>
 
 <style scoped>
@@ -60,5 +26,6 @@ const formattedMessages = computed(() => {
 .slide-enter-from,
 .slide-leave-to {
   transform: translateY(-150%);
+  /* opacity: 0; */
 }
 </style>
--- src/composables/messages.js
+++ src/composables/messages.js
@@ -3,14 +3,34 @@ let messages = ref([]);
 export default function useMessages() {
   function addToMessages(type, query = "") {
     messages.value.push({
-      type: type,
-      query: query,
+      title: titles[type],
+      description: `${query} ${descriptions[type]}`,
     });
 
-    // setTimeout(() => {
-    //   messages.value.shift();
-    // }, 2500);
+    setTimeout(() => {
+      messages.value.shift();
+    }, 2500);
   }
 
   return { messages, addToMessages };
 }
+
+const titles = {
+  gender: "Student was not made prefect!",
+  house: "Student was not made prefect!",
+  inquisitor: "Student was not made an inquisitor!",
+  hacking: "Student was not made an inquisitor!",
+  expulsion1: "Student could not be expelled!",
+  expulsion2: "Student could not be expelled!",
+  expulsion3: "Student could not be expelled!",
+};
+
+const descriptions = {
+  gender: "already has a prefect of that gender.",
+  house: "already has both of their prefects.",
+  inquisitor: "is neither a Slytherin nor pure of blood.",
+  hacking: "has been removed as an inquisitor.",
+  expulsion1: "Do not try to expel this student again.",
+  expulsion2: "I said do NOT try to expel this student again!",
+  expulsion3: "This is the last warning. Do NOT expel this student!",
+};
